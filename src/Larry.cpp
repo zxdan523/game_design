f0883cfd25861e3554ee8a73d4e98a0dc869e470
Jonathan <jlin04@email.wm.edu>
2017-11-12 23:28:38 -0500

    merge conflicts

--- src/Larry.cpp
+++ src/Larry.cpp
@@ -166,8 +166,6 @@ void Larry::draw(sf::RenderTarget& target,sf::RenderStates states) const
     target.draw(_back_fin,states);
     target.draw(_tail,states);
 
-<<<<<<< HEAD
-=======
     /*for(int i=0;i<_knots.size();i++)
     {
         sf::CircleShape circle(5.0f);
@@ -175,5 +173,5 @@ void Larry::draw(sf::RenderTarget& target,sf::RenderStates states) const
         circle.setPosition(_knots[i].getPosition());
         target.draw(circle,states);
     }*/
->>>>>>> 3ea49e2204eb6296fc6201020218d412dd3a2b48
+
 }
