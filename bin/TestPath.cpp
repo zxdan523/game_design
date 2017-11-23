299f48dde78239ffb674aefbb5b0c37659a1f5e9
Jonathan <jlin04@email.wm.edu>
2017-11-23 00:32:37 -0500

    path testing

--- bin/TestPath.cpp
+++ bin/TestPath.cpp
@@ -2,9 +2,15 @@
 #include <SFML/Audio.hpp>
 #include "Minions.h"
 #include "XMLParser.h"
+#include "Larry.h"
 #include "TextureManager.h"
+#include "TileMap.h"
 #include <iostream>
 #include "util.h"
+
+sf::Vector2f getPath(sf::Vector2i dest) {
+	return sf::Vector2f(dest.x + 100,dest.y + 100);	
+}
 
 int main(int argc, char** argv)
 {
@@ -12,20 +18,29 @@ int main(int argc, char** argv)
   // Resource Managers
 	TextureManager textures;
 	XMLParser parser;
-
+	TileMap map;
 
   // create main window
 	sf::RenderWindow App(sf::VideoMode(1024,768,32), "Path - SFML",sf::Style::Titlebar|sf::Style::Close);
-	Minions fish;
+	Minions minion;
+	Larry larry;
 
 	parser.loadXML("../data/xml/TestXML.xml");
 	parser.loadTexture(textures, "fish");
+	std::vector<int> tiles = parser.getTileMap();
+	parser.loadTexture(textures,"terrain");
+  	if (!map.load(textures,"grass", sf::Vector2u(64, 64), tiles, 16, 12))
+    	return -1;
+
 
 	
-	fish.setTexture(textures.get("fish").texture);
-	fish.setTextureAreas(textures.get("fish").areas);
+	minion.setTexture(textures.get("fish").texture);
+	minion.setTextureAreas(textures.get("fish").areas);
+	larry.setTexture(textures.get("fish").texture);
+	larry.setTextureAreas(textures.get("fish").areas);
 
-	fish.init();
+	minion.init();
+	larry.init();
     // start main loop
 	sf::Clock clock;
 	while(App.isOpen())
@@ -39,28 +54,36 @@ int main(int argc, char** argv)
                 break;
             case sf::Event::KeyPressed:
                     if(event.key.code==sf::Keyboard::A)
-                        fish.recovered();
+                        minion.recovered();
                     if(event.key.code==sf::Keyboard::S)
-                        fish.shot();
+                        minion.shot();
                     if(event.key.code==sf::Keyboard::D)
-                        fish.shocked();
+                        minion.shocked();
                     if(event.key.code==sf::Keyboard::F)
-                        fish.weaken();
+                        minion.weaken();
                 break;
             default:
                 break;
         }
+    }
     // clear screen and fill with blue
 		App.clear(sf::Color::Blue);
 		
 		sf::Vector2i mousePos=sf::Mouse::getPosition(App);
-		fish.swimTo(sf::Vector2f(mousePos.x,mousePos.y));
-		fish.update(0);
-		App.draw(fish);
+		//minion.swimTo(sf::Vector2f(mousePos.x,mousePos.y));
+		minion.swimTo(getPath(mousePos));
+		minion.update(0);
+		larry.swimTo(sf::Vector2f(mousePos.x,mousePos.y));
+		larry.update(0);
+		App.draw(map);
+		App.draw(larry);
+		App.draw(minion);
 		App.display();
 	}
 
+
   // Done.
 	return 0;
+
 }
-}
+
