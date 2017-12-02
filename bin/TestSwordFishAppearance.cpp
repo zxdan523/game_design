cf64cb8eefbf3a7ab3fcd8b28bd846161371abb8
jwei22 <jimmy.wei22@gmail.com>
2017-12-02 18:25:57 -0500

    test appearances

--- bin/TestSwordFishAppearance.cpp
+++ bin/TestSwordFishAppearance.cpp
@@ -50,9 +50,20 @@ int main(int argc, char** argv)
                     break;
                 case sf::Event::KeyPressed:
                     if (event.key.code == sf::Keyboard::A) {
+                        positions[0] = sf::Vector2f(1,1);
+                        positions[1] = sf::Vector2f(200,1);
+                        positions[2] = sf::Vector2f(400,1);
+                        positions[3] = sf::Vector2f(600,1);
+                        positions[4] = sf::Vector2f(800,1);
+                        
+                        directions[0] = sf::Vector2f(1.0f,1.0f);
+                        directions[1] = sf::Vector2f(1.0f,1.0f);
+                        directions[2] = sf::Vector2f(1.0f,1.0f);
+                        directions[3] = sf::Vector2f(1.0f,1.0f);
+                        directions[4] = sf::Vector2f(1.0f,1.0f);
                         for(int i=0;i<5;i++)
                         {
-                            fish[i]=std::make_shared<Swordfish>(sf::Vector2f(i*200+1,1),sf::Vector2f(1.0f,1.0f));
+                            fish[i]=std::make_shared<Swordfish>(positions[i],directions[i]);
                             fish[i]->setTexture(textures.get().texture);
                             fish[i]->setTextureAreas(textures.get().areas);
                             fish[i]->init();
@@ -60,16 +71,76 @@ int main(int argc, char** argv)
                     }
                     if (event.key.code == sf::Keyboard::S) {
                         positions[0] = sf::Vector2f(200,1);
-                        positions[1] = sf::Vector2f(1,200);
-                        positions[2] = sf::Vector2f(400,1);
-                        positions[3] = sf::Vector2f(1,400);
-                        positions[4] = sf::Vector2f(600,1);
+                        positions[1] = sf::Vector2f(1,400);
+                        positions[2] = sf::Vector2f(500,1);
+                        positions[3] = sf::Vector2f(1,700);
+                        positions[4] = sf::Vector2f(800,1);
                         
                         directions[0] = sf::Vector2f(1.0f,1.0f);
                         directions[1] = sf::Vector2f(1.0f,0.5f);
                         directions[2] = sf::Vector2f(0.5f,1.0f);
                         directions[3] = sf::Vector2f(1.0f,-0.25f);
-                        directions[4] = sf::Vector2f(1.0f,1.0f);
+                        directions[4] = sf::Vector2f(-1.0f,1.0f);
+                        for(int i=0;i<5;i++)
+                        {
+                            fish[i]=std::make_shared<Swordfish>(positions[i],directions[i]);
+                            fish[i]->setTexture(textures.get().texture);
+                            fish[i]->setTextureAreas(textures.get().areas);
+                            fish[i]->init();
+                        }
+                    }
+                    if (event.key.code == sf::Keyboard::D) {
+                        positions[0] = sf::Vector2f(1,WINDOW_HEIGHT);
+                        positions[1] = sf::Vector2f(200,WINDOW_HEIGHT);
+                        positions[2] = sf::Vector2f(400,WINDOW_HEIGHT);
+                        positions[3] = sf::Vector2f(600,WINDOW_HEIGHT);
+                        positions[4] = sf::Vector2f(800,WINDOW_HEIGHT);
+                        
+                        directions[0] = sf::Vector2f(1.0f,-1.0f);
+                        directions[1] = sf::Vector2f(1.0f,-1.0f);
+                        directions[2] = sf::Vector2f(1.0f,-1.0f);
+                        directions[3] = sf::Vector2f(1.0f,-1.0f);
+                        directions[4] = sf::Vector2f(1.0f,-1.0f);
+                        for(int i=0;i<5;i++)
+                        {
+                            fish[i]=std::make_shared<Swordfish>(positions[i],directions[i]);
+                            fish[i]->setTexture(textures.get().texture);
+                            fish[i]->setTextureAreas(textures.get().areas);
+                            fish[i]->init();
+                        }
+                    }
+                    if (event.key.code == sf::Keyboard::F) {
+                        positions[0] = sf::Vector2f(1,21);
+                        positions[1] = sf::Vector2f(WINDOW_WIDTH-20,1);
+                        positions[2] = sf::Vector2f(21,WINDOW_HEIGHT);
+                        positions[3] = sf::Vector2f(WINDOW_WIDTH,WINDOW_HEIGHT-20);
+                        positions[4] = sf::Vector2f(WINDOW_WIDTH/2,1);
+                        
+                        directions[0] = sf::Vector2f(1.0f,1.0f);
+                        directions[1] = sf::Vector2f(-1.0f,1.0f);
+                        directions[2] = sf::Vector2f(1.0f,-1.0f);
+                        directions[3] = sf::Vector2f(-1.0f,-1.0f);
+                        directions[4] = sf::Vector2f(0.0f,1.0f);
+                        for(int i=0;i<5;i++)
+                        {
+                            fish[i]=std::make_shared<Swordfish>(positions[i],directions[i]);
+                            fish[i]->setTexture(textures.get().texture);
+                            fish[i]->setTextureAreas(textures.get().areas);
+                            fish[i]->init();
+                        }
+                    }
+                    if (event.key.code == sf::Keyboard::G) {
+                        positions[0] = sf::Vector2f(WINDOW_WIDTH/3,1);
+                        positions[1] = sf::Vector2f(WINDOW_WIDTH*2/3,1);
+                        positions[2] = sf::Vector2f(1,WINDOW_HEIGHT/4);
+                        positions[3] = sf::Vector2f(1,WINDOW_HEIGHT*2/4);
+                        positions[4] = sf::Vector2f(1,WINDOW_HEIGHT*3/4);
+                        
+                        directions[0] = sf::Vector2f(0.0f,1.0f);
+                        directions[1] = sf::Vector2f(0.0f,1.0f);
+                        directions[2] = sf::Vector2f(1.0f,0.0f);
+                        directions[3] = sf::Vector2f(1.0f,0.0f);
+                        directions[4] = sf::Vector2f(1.0f,0.0f);
                         for(int i=0;i<5;i++)
                         {
                             fish[i]=std::make_shared<Swordfish>(positions[i],directions[i]);
